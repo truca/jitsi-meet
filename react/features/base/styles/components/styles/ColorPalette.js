344a07cb20976efee4bd20b5390ea99f6132fe57
Ignacio Ureta <iureta@evernote.com>
2020-04-21 07:24:07 -0400

    feat(colors) changed colors

--- react/features/base/styles/components/styles/ColorPalette.js
+++ react/features/base/styles/components/styles/ColorPalette.js
@@ -19,8 +19,8 @@ export const ColorPalette = {
      */
     black: BLACK,
     blackBlue: 'rgb(0, 3, 6)',
-    blue: '#17A0DB',
-    blueHighlight: '#1081b2',
+    blue: '#5202c4',
+    blueHighlight: '#060078',
     buttonUnderlay: '#495258',
     darkGrey: '#555555',
     green: '#40b183',
